24941bb7bded4a38f3bf52dba3f85d5050c7976a
Cole <bleeper_genius0c@icloud.com>
2023-12-08 14:19:18 -0600

    Linter

--- C/2023/day08.c
+++ C/2023/day08.c
@@ -4,17 +4,18 @@
 #include <ctype.h>
 #include <advent.h>
 
-typedef struct {
+typedef struct
+{
     char k[4];
     char l[4];
     char r[4];
 } Node;
 
-char* generator = NULL;
-Node* tree = NULL;
+char *generator = NULL;
+Node *tree = NULL;
 size_t e = 0;
 
-Node* get(const char* k)
+Node *get(const char *k)
 {
     size_t i = 0;
     while (i < e && strcmp(tree[i].k, k) != 0)
@@ -22,7 +23,7 @@ Node* get(const char* k)
     return i < e ? &tree[i] : NULL;
 }
 
-void build_tree(FILE* fp)
+void build_tree(FILE *fp)
 {
     // get generator
     generator = malloc(1000 * sizeof(char));
@@ -40,7 +41,7 @@ void build_tree(FILE* fp)
     e = i;
 }
 
-long parse(char** s, size_t l)
+long parse(char **s, size_t l)
 {
     // make a map of successes
     long f = 0, idx = 0;
@@ -51,13 +52,14 @@ long parse(char** s, size_t l)
         // for each item, send it left or right (or mark it successful)
         for (size_t i = 0; i < l; ++i)
         {
-            if (s[i][strlen(s[i])-1] == 'Z')
+            if (s[i][strlen(s[i]) - 1] == 'Z')
                 k[f++] = idx;
             if (generator[idx % strlen(generator)] == 'R')
                 s[i] = get(s[i])->r;
             else if (generator[idx % strlen(generator)] == 'L')
                 s[i] = get(s[i])->l;
-            else {
+            else
+            {
                 printf("Bad character in generator: %c\n", generator[idx % strlen(generator)]);
                 exit(EXIT_FAILURE);
             }
@@ -70,34 +72,34 @@ long parse(char** s, size_t l)
 
 int main()
 {
-	FILE *fp = fopen("day08.txt", "r");
-	if (!fp)
-	{
-		printf("Bad file read\n");
-		exit(EXIT_FAILURE);
-	}
+    FILE *fp = fopen("day08.txt", "r");
+    if (!fp)
+    {
+        printf("Bad file read\n");
+        exit(EXIT_FAILURE);
+    }
 
     build_tree(fp);
 
     // part 1 - start at just AAA
-    char* f[1] = {"AAA"};
-	printf("ONE: %ld\n", parse(f, 1));
+    char *f[1] = {"AAA"};
+    printf("ONE: %ld\n", parse(f, 1));
 
     // part 2 - start at all items ending in A
     size_t i = 0;
-    char** two = malloc(e * sizeof(char*));
+    char **two = malloc(e * sizeof(char *));
     for (size_t j = 0; j < e; ++j)
     {
         if (tree[j].k[2] == 'A')
             two[i++] = tree[j].k;
     }
-    two = realloc(two, i * sizeof(char*));
+    two = realloc(two, i * sizeof(char *));
     if (!two)
         exit(EXIT_FAILURE);
-	printf("TWO: %ld\n", parse(two, i));
+    printf("TWO: %ld\n", parse(two, i));
 
     free(generator);
     free(tree);
-	fclose(fp);
-	return 0;
+    fclose(fp);
+    return 0;
 }
