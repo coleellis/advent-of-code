4b6edf67d6f6a628572fbb93b783df2f76349398
Cole <bleeper_genius0c@icloud.com>
2024-12-02 08:04:33 -0500

    C/2024/02

--- C/2024/day02.c
+++ C/2024/day02.c
@@ -1,30 +1,30 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int safe(int* row, int len)
+bool safe(int *row, size_t len)
 {
-    int safe = 1;
-    int inc = 1;
+    bool safe = true;
+    bool inc = true;
 
-    for (int i = 0; i < len - 1; ++i) {
-        // First pass -- Set increasing flag
-        if (i == 0) {
-            if (row[i + 1] - row[i] > 0)
-                inc = 1;
-            else
-                inc = 0;
-        }
+    for (size_t i = 0; i < len - 1; ++i)
+    {
+        // Set increment on first pass
+        if (i == 0)
+            inc = (row[i + 1] > row[i]);
 
-        // Check for swap
-        if ((inc && row[i + 1] < row[i]) || (!inc && row[i + 1] > row[i])) {
-            safe = 0;
+        // Check for increment swap
+        if ((inc && row[i + 1] < row[i]) || (!inc && row[i + 1] > row[i]))
+        {
+            safe = false;
             break;
         }
 
-        // Check for bounds
-        if (abs(row[i + 1] - row[i]) < 1 || abs(row[i + 1] - row[i]) > 3) {
-            safe = 0;
+        // Check bounds
+        if (abs(row[i + 1] - row[i]) < 1 || abs(row[i + 1] - row[i]) > 3)
+        {
+            safe = false;
             break;
         }
     }
@@ -32,17 +32,21 @@ int safe(int* row, int len)
     return safe;
 }
 
-int* tokenize(char* line, size_t* len)
+int *tokenize(char *line, size_t *len)
 {
     size_t i = 0;
-    int* arr = calloc(++i, sizeof(int));
+    int *arr = calloc(++i, sizeof(int));
 
-    char* ptr;
-    char* token = strtok_r(line, " ", &ptr);
-    while (token) {
-        long i = strtol(token, NULL, 10);
-        arr[i - 1] = (int)i;
-        arr = realloc(arr, (++i) * sizeof(int));
+    char *ptr;
+    char *token = strtok_r(line, " ", &ptr);
+    while (token)
+    {
+        // Get number
+        long num = strtol(token, NULL, 10);
+        arr[i - 1] = (int)num;
+
+        // Resize and get next
+        arr = reallocarray(arr, ++i, sizeof(int));
         token = strtok_r(NULL, " ", &ptr);
     }
 
@@ -50,33 +54,60 @@ int* tokenize(char* line, size_t* len)
     return arr;
 }
 
-int main(int argc, char** argv)
+int *pop(int *row, size_t len, size_t idx)
 {
-    // Validate Arguments
-    if (argc != 2) {
+    // Make a copy
+    int *copy = calloc(len - 1, sizeof(int));
+
+    // Copy
+    memcpy(copy, row, idx * sizeof(int));
+    memcpy(&copy[idx], &row[idx + 1], (len - idx - 1) * sizeof(int));
+
+    return copy;
+}
+
+int main(int argc, char **argv)
+{
+    // Args
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Get File
-    FILE* fp = fopen(argv[1], "r");
-    if (!fp) {
+    // Get filename
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp)
+    {
         perror("fopen");
         exit(EXIT_FAILURE);
     }
 
-    char* line = NULL;
-    size_t len = 0;
-    size_t one = 0;
-    while (getline(&line, &len, fp) != -1) {
-        int* row = tokenize(line, &len);
+    // Run through
+    char *line = NULL;
+    size_t len;
+    int one = 0;
+    int two = 0;
+    while (getline(&line, &len, fp) != -1)
+    {
+        // Part 1
+        int *row = tokenize(line, &len);
         if (safe(row, len))
             ++one;
+
+        // Part 2
+        for (size_t i = 0; i < len; ++i)
+        {
+            int *newrow = pop(row, len, i);
+            if (safe(newrow, len - 1))
+                ++two;
+            free(newrow);
+        }
         free(row);
     }
 
-    // Print Answers
-    printf("ONE: %zu\n", one);
+    printf("one: %d\n", one);
+    printf("two: %d\n", two);
 
     fclose(fp);
     if (line)
